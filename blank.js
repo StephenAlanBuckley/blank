62d50e1ebd4b3ef7560a4b12e52673faee6b4ef8
Stephen Buckley <stephen.alan.buckley@gmail.com>
2014-05-07 11:56:45 -0400

    some HN comments

--- blank.js
+++ blank.js
@@ -2,6 +2,7 @@ $(".title a").prop('target', '_blank');
 $(".mode-night a").css('color', '#ddd');
 $(".mode-night a").css('text-decoration', 'none');
 
+//if we're on HackerNews they don't use classes much so it gets more awkward
 if (window.location.href.indexOf("ycombinator") > -1) {
   $($("a").get().reverse()).each(function() {
     if ($(this).text() === "More") {
@@ -9,4 +10,8 @@ if (window.location.href.indexOf("ycombinator") > -1) {
       return false;
     }
   });
+
+  //I'd like to add collapsible comments. There's a good guide here:
+  //http://alexander.kirk.at/js/hackernews-collapsible-threads.js
+  //But I'm at work now and should get some other stuff done
 }
